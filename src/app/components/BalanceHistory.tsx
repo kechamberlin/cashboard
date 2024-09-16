8473b1aab875f31d744a3ef7cc3117d18d026a2b
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2024-09-16 16:09:44 -0700

    (WIP) Initial UI mock up / ongoing UI changes.

--- src/app/components/BalanceHistory.tsx
+++ src/app/components/BalanceHistory.tsx
@@ -1,25 +1,38 @@
 'use client';
 
-import { Card, Text } from '@mantine/core';
-import { BarChart } from '@mantine/charts';
+import { Card, Space, Text } from '@mantine/core';
+import { BarChart, LineChart } from '@mantine/charts';
 
 const BalanceHistory = () => {
   const MOCK_DATA = [
     // TODO: Define a data structure, ex:
     //    { month: '', year: ??, balance: 1014.21, }
 
-    { month: 'January', balance: 25225.34 },
-    { month: 'February', balance: 26582.67 },
-    { month: 'March', balance: 30709.25 },
-    { month: 'April', balance: 36553.71 },
-    { month: 'May', balance: 41018.08 },
-    { month: 'June', balance: 43661.69 },
-    { month: 'July', balance: 43035.39 },
-    { month: 'August', balance: 39540.19 },
-    { month: 'September', balance: 42702.68 },
-    { month: 'October', balance: 47936.54 },
-    { month: 'November', balance: 48092.91 },
-    { month: 'December', balance: 59791.99 },
+    // { month: 'January', balance: 25225.34 },
+    // { month: 'February', balance: 26582.67 },
+    // { month: 'March', balance: 30709.25 },
+    // { month: 'April', balance: 36553.71 },
+    // { month: 'May', balance: 41018.08 },
+    // { month: 'June', balance: 43661.69 },
+    // { month: 'July', balance: 43035.39 },
+    // { month: 'August', balance: 39540.19 },
+    // { month: 'September', balance: 42702.68 },
+    // { month: 'October', balance: 47936.54 },
+    // { month: 'November', balance: 48092.91 },
+    // { month: 'December', balance: 59791.99 },
+
+    { month: 'January', checking: 10000, savings: 20000, investments: 50000 },
+    { month: 'February', checking: 11000, savings: 21000, investments: 42000 },
+    { month: 'March', checking: 12000, savings: 22000, investments: 45000 },
+    { month: 'April', checking: 13000, savings: 24000, investments: 53000 },
+    { month: 'May', checking: 14000, savings: 24000, investments: 60000 },
+    { month: 'June', checking: 15000, savings: 25000, investments: 58000 },
+    { month: 'July', checking: 14000, savings: 24000, investments: 52000 },
+    { month: 'August', checking: 13000, savings: 26000, investments: 55000 },
+    { month: 'September', checking: 16000, savings: 24500, investments: 62000 },
+    { month: 'October', checking: 18000, savings: 27000, investments: 60000 },
+    { month: 'November', checking: 20000, savings: 29000, investments: 64000 },
+    { month: 'December', checking: 22000, savings: 29950, investments: 65000 },
   ];
 
   return (
@@ -32,9 +45,30 @@ const BalanceHistory = () => {
         data={MOCK_DATA}
         dataKey='month'
         valueFormatter={(value) => new Intl.NumberFormat('en-US').format(value)}
-        series={[{ name: 'balance', color: 'cyan' }]}
+        // series={[{ name: 'balance', label: 'Balance', color: 'cyan' }, ]}
+        series={[
+          { name: 'checking', label: 'Checking', color: 'cyan.3' },
+          { name: 'savings', label: 'Savings', color: 'cyan.6' },
+          { name: 'investments', label: 'Investments', color: 'cyan.9' },
+        ]}
         barProps={{ radius: 8 }}
+        type='stacked'
       />
+      {/* <LineChart
+        h={400}
+        data={MOCK_DATA}
+        dataKey='month'
+        valueFormatter={(value) => new Intl.NumberFormat('en-US').format(value)}
+        // series={[{ name: 'balance' }]}
+        series={[
+          { name: 'checking', label: 'Checking', color: 'red' },
+          { name: 'savings', label: 'Savings', color: 'green' },
+          // { name: 'investments', label: 'Investments', color: 'cyan.9' },
+        ]}
+        curveType='natural'
+        connectNulls
+        // strokeWidth={3}
+      /> */}
     </Card>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,21 @@ import BalanceHistory from './components/BalanceHistory';
 import TotalBalance from './components/TotalBalance';
 import Investments from './components/Investments';
 import CreditScore from './components/CreditScore';
-import { Container, Grid, GridCol, Group, List, ListItem } from '@mantine/core';
+import { Card, Container, Grid, GridCol, Group, List, ListItem } from '@mantine/core';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Cashboard',
+  description: 'A personal finance application for those who prefer to micromanage their budget.',
+};
 
 export default function Home() {
   return (
     <Container mih={'100vh'} p={0} fluid bd={0}>
       <Grid grow gutter={0}>
-        <GridCol span={2} bg={'white'} p={32} mih={'100vh'}>
+        {/* TODO: mih={'100vh'} should be for desktop only.  Responsive view should be icons / collapsed. */}
+        {/* TODO: move Side Drawer to Layout */}
+        <GridCol span={2} p={32} mih={'100vh'}>
           <List withPadding listStyleType='none'>
             <ListItem>Income</ListItem>
             <ListItem>Expenses</ListItem>
@@ -31,61 +39,64 @@ export default function Home() {
           </List>
         </GridCol>
 
-        <GridCol span={10} p={32}>
-          <Group grow gap={0} justify='flex-start' align='stretch' style={{ flexDirection: 'column' }} h={'100%'}>
-            {/* Income, Expenses, Saved */}
-            <Group grow>
-              <Grid justify='flex-start'>
-                <GridCol span={4}>
-                  <Link href={`/income`}>
-                    <Income />
-                  </Link>
-                </GridCol>
-                <GridCol span={4}>
-                  <Link href={`/expenses`}>
-                    <Expenses />
-                  </Link>
-                </GridCol>
-                <GridCol span={4}>
-                  <Link href={`/savings`}>
-                    <Saved />
-                  </Link>
-                </GridCol>
-              </Grid>
-            </Group>
+        <GridCol span={10} p={10}>
+          <Card radius={25} bg={'gray.1'} h={'100%'}>
+            <Group grow gap={0} justify='flex-start' align='stretch' style={{ flexDirection: 'column' }} h={'100%'} p={'md'}>
+              {/* Income, Expenses, Saved */}
+              <Group grow align='center'>
+                <Grid>
+                  <GridCol span={4}>
+                    <Link href={`/income-and-expenses`}>
+                      <Income />
+                    </Link>
+                  </GridCol>
+                  <GridCol span={4}>
+                    <Link href={`/income-and-expenses`}>
+                      <Expenses />
+                    </Link>
+                  </GridCol>
+                  <GridCol span={4}>
+                    <Link href={`/savings`}>
+                      <Saved />
+                    </Link>
+                  </GridCol>
+                </Grid>
+              </Group>
 
-            {/* Balance History, Total Balance  */}
-            <Group grow>
-              <Grid justify='flex-start'>
-                <GridCol span={8}>
-                  <Link href={`/balance-history`}>
-                    <BalanceHistory />
-                  </Link>
-                </GridCol>
-                <GridCol span={4}>
-                  <Link href={`/total-balance`}>
-                    <TotalBalance />
-                  </Link>
-                </GridCol>
-              </Grid>
-            </Group>
+              {/* Balance History, Total Balance  */}
+              <Group grow align='flex-start'>
+                <Grid>
+                  <GridCol span={8}>
+                    <Link href={`/balance-history`}>
+                      <BalanceHistory />
+                    </Link>
+                  </GridCol>
+                  <GridCol span={4}>
+                    <Link href={`/total-balance`}>
+                      <TotalBalance />
+                    </Link>
+                  </GridCol>
+                </Grid>
+              </Group>
 
-            {/* Investments, Credit Score */}
-            <Group grow>
-              <Grid justify='flex-start'>
-                <GridCol span={6}>
-                  <Link href={`/investments`}>
-                    <Investments />
-                  </Link>
-                </GridCol>
-                <GridCol span={6}>
-                  <Link href={`/credit-score`}>
-                    <CreditScore />
-                  </Link>
-                </GridCol>
-              </Grid>
+              {/* Investments, Credit Score */}
+              <Group grow align='flex-start'>
+                <Grid>
+                  <GridCol span={6}>
+                    <Link href={`/investments`}>
+                      <Investments />
+                    </Link>
+                  </GridCol>
+                  <GridCol span={6}>
+                    <Link href={`/credit-score`}>
+                      <CreditScore />
+                    </Link>
+                  </GridCol>
+                </Grid>
+              </Group>
             </Group>
-          </Group>
+            {/* end card */}
+          </Card>
         </GridCol>
       </Grid>
     </Container>
