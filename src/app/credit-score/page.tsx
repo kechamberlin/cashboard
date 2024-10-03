3311093512d76cf1b302aadf59f4ac140e8a2bb3
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2024-10-02 17:44:47 -0700

    Add preliminary DonutChart.

--- src/app/credit-score/page.tsx
+++ src/app/credit-score/page.tsx
@@ -1,8 +1,29 @@
+import { DonutChart } from '@mantine/charts';
+
 const CreditScorePage = () => {
   return (
     <>
       <h2>Credit Score</h2>
       <p>Credit Score Page</p>
+      <DonutChart
+        data={[
+          { name: 'Poor', value: 579, color: 'red.1' },
+          { name: 'Fair', value: 669, color: 'orange.1' },
+          { name: 'Good', value: 739, color: 'yellow.1' },
+          { name: 'Very Good', value: 799, color: 'green.3' },
+          { name: 'Exceptional', value: 850, color: 'green.9' },
+        ]}
+        thickness={35}
+        strokeWidth={3}
+        tooltipDataSource='segment'
+        w={'100%'}
+        startAngle={180}
+        endAngle={0}
+        chartLabel={799}
+        // withTooltip={false}
+        withLabelsLine
+        withLabels
+      />
     </>
   );
 };
