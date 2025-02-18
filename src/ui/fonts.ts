1a6b1e405a0c89e4e866ed46c33db5c9faf7e9fb
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2025-02-18 11:05:16 -0800

    Created fonts file for preliminary app fonts.

---
+++ src/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Lexend, Lusitana } from 'next/font/google';
+
+export const lexend = Lexend({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
+
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
