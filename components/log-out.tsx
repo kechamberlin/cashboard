ec242880cccf9237db685c0898b4fb229a985760
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2025-04-06 14:48:29 -0700

    Replaced sidebar avatar with log out button and renamed file.

---
+++ components/log-out.tsx
@@ -0,0 +1,30 @@
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import {
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '@/components/ui/sidebar';
+import Link from 'next/link';
+import { LogOut } from 'lucide-react';
+
+export function NavUser() {
+  return (
+    <SidebarMenu>
+      <SidebarMenuItem>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <SidebarMenuButton size={'lg'} className='font-bold' asChild>
+              <Link href={'/'}>
+                <LogOut />
+                <span>Log Out</span>
+              </Link>
+            </SidebarMenuButton>
+          </DropdownMenuTrigger>
+        </DropdownMenu>
+      </SidebarMenuItem>
+    </SidebarMenu>
+  );
+}
