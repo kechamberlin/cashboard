a36115530beb10f73d0bdcb1a316305dea12cd95
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2025-04-06 17:26:28 -0700

    Created application pages.

---
+++ app/cashboard/debt/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Debt',
+};
+
+export default function Page() {
+  return (
+    <>
+      <h1>Debt</h1>
+    </>
+  );
+}
---
+++ app/cashboard/income-and-expenses/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Income and Expenses',
+};
+
+export default function Page() {
+  return (
+    <>
+      <h1>Income and Expenses</h1>
+    </>
+  );
+}
