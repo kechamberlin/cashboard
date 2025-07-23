f9ed3a2974b7aabef55133460e71b213aa2ca4f0
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2025-07-23 12:33:12 -0700

    Initial setup for Tanstack React Query.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import Providers from './providers';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -28,10 +29,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <head />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-cashboard-indigo-base`}
       >
-        {children}
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
